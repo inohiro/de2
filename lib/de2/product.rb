91d4a72fc8d660e72f8080431d2975f563d87c44
Hiroyuki Inoue <mammymax@gmail.com>
2012-11-30 11:28:13 +0900

    動いた版

--- lib/de2/product.rb
+++ lib/de2/product.rb
@@ -2,22 +2,48 @@
 module DE2
   class Product
 
-    attr_reader :url
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8)
+    AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75
+    Safari/535.7"
+
+    attr_reader :url, :star
 
     def initialize( url )
       @url = url
-      @body = Nokogiri::HTML( open( self.url ) )
+      @body = Nokogiri::HTML( open( self.url,"User-Agent" => USER_AGENT ) )
+      @star = _get_star
     end
 
-    def star
-      _get_star
+    def similar_products
+      products = @body.xpath( '//*[@class="acc-product"]' )
+      puts "products.size: #{products.size}"
+      if products.size >= 1
+        ary = []
+        products.each do |product|
+          ary << _get_url( product )
+        end
+        ary
+      else
+        []
+      end
     end
 
     private
 
+    def _get_url( product )
+      a = product.xpath( 'a' ).first
+      url = a.xpath( '@href' ).first.value
+      title = a.xpath( '@title' ).first.value
+      { :title => title, :url => url, :star => 0.0 }
+    end
+
+    def _get_sim_products
+
+    end
+
     def _get_star
       res = @body.xpath( '//div[@class="gry txtnormal acrRating"]' )
-      if res.length == 1
+      if res.size == 1
         match = res.first.inner_text.match( ' ' )
         match !=nil ? match.post_match.to_f : nil
       else
--- main.rb
+++ main.rb
@@ -6,16 +6,34 @@ require 'de2'
 require 'pp'
 
 def main
-  id  = 'B009C6W1PA'
-#  url = 'http://www.amazon.co.jp/dp/B007G3SW86/'
-  url = 'http://www.amazon.co.jp/dp/B009C6W1PA'
+
+  url = 'http://www.amazon.co.jp/dp/B0067PGV1O/'
 
   product = DE2::Product.new( url )
   star = product.star
+  products = product.similar_products
 
-  pp star
+  tmp = products.dup
+  products.each do |p|
+    puts p[:title]
+    np = DE2::Product.new( p[:url] )
+    np_star = np.star
+    p[:star] = np_star if np_star
 
+    hoge = np.similar_products
+    hoge.each do |h|
+      nh = DE2::Product.new( h[:url] )
+      nh_star = nh.star
+      h[:star] = nh.star if nh_star
+    end
 
+    tmp << hoge
+    tmp.flatten!
+  end
+
+  p '**********************************'
+  pp tmp.sort_by { |e| e[:star] }.reverse.uniq.slice(0..5)
+  puts tmp.size
 end
 
 main
